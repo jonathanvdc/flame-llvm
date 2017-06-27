c7cdd2ce7c8fb089d371e579c3eaa69e36fdbf0c
jonathanvdc <jonathan.vdc@outlook.com>
2017-06-27 17:12:30 +0200

    Support dynamically linking to the system libLLVM.so

--- build-libs.sh
+++ build-libs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-if [ -z "$1" ]; then
-  echo error: too few arguments
-  echo usage: build-libs.sh /path/to/libLLVM.so
-  exit 1
-fi
-
+rm -rf native-libs
 mkdir -p native-libs
-cp $1 native-libs/libLLVM.so
+if [ -z "$1" ]; then
+  echo warning: no native library will be copied to the output directory
+  echo usage: build-libs.sh [/path/to/libLLVM.so]
+else
+  cp $1 native-libs/libLLVM.so
+fi
 
 mkdir -p libs
 mcs /target:library /out:libs/LLVMSharp.dll LLVMSharp/*.cs
