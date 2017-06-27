9e1db24715278332c4c786edcc1314ce66c50630
jonathanvdc <jonathan.vdc@outlook.com>
2017-06-27 14:51:49 +0200

    Copy libLLVM.so to the output dir

--- build-libs.sh
+++ build-libs.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+if [ -z "$1" ]; then
+  echo error: too few arguments
+  echo usage: build-libs.sh /path/to/libLLVM.so
+  exit 1
+fi
+
+mkdir -p native-libs
+cp $1 native-libs/libLLVM.so
+
 mkdir -p libs
 mcs /target:library /out:libs/LLVMSharp.dll LLVMSharp/*.cs
