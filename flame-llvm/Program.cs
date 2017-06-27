258eb8727dcc1fc73006c5400369ef9376243d3a
jonathanvdc <jonathan.vdc@outlook.com>
2017-06-27 13:17:48 +0200

    Initial commit

---
+++ flame-llvm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using LLVMSharp;
+using System.Runtime.InteropServices;
+
+namespace Flame.Llvm
+{
+    public static class Program
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int Add(int a, int b);
+
+        public static void Main(string[] args)
+        {
+            LLVMBool False = new LLVMBool(0);
+            LLVMModuleRef mod = LLVM.ModuleCreateWithName("LLVMSharpIntro");
+
+            LLVMTypeRef[] param_types = {LLVM.Int32Type(), LLVM.Int32Type()};
+            LLVMTypeRef ret_type = LLVM.FunctionType(LLVM.Int32Type(), out param_types[0], 2, False);
+            LLVMValueRef sum = LLVM.AddFunction(mod, "sum", ret_type);
+
+            LLVMBasicBlockRef entry = LLVM.AppendBasicBlock(sum, "entry");
+
+            LLVMBuilderRef builder = LLVM.CreateBuilder();
+            LLVM.PositionBuilderAtEnd(builder, entry);
+            LLVMValueRef tmp = LLVM.BuildAdd(builder, LLVM.GetParam(sum, 0), LLVM.GetParam(sum, 1), "tmp");
+            LLVM.BuildRet(builder, tmp);
+
+            IntPtr error;
+            LLVM.VerifyModule(mod, LLVMVerifierFailureAction.LLVMAbortProcessAction, out error);
+            LLVM.DisposeMessage(error);
+
+            LLVMExecutionEngineRef engine;
+
+            LLVM.LinkInMCJIT();
+            LLVM.InitializeNativeTarget();
+            LLVM.InitializeNativeAsmPrinter();
+
+            var options = new LLVMMCJITCompilerOptions();
+            var optionsSize = (4*sizeof (int)) + IntPtr.Size; // LLVMMCJITCompilerOptions has 4 ints and a pointer
+
+            LLVM.InitializeMCJITCompilerOptions(out options, optionsSize);
+            LLVM.CreateMCJITCompilerForModule(out engine, mod, out options, optionsSize, out error);
+
+            var addMethod = (Add) Marshal.GetDelegateForFunctionPointer(LLVM.GetPointerToGlobal(engine, sum), typeof (Add));
+            int result = addMethod(10, 10);
+
+            Console.WriteLine("Result of sum is: " + result);
+
+            if (LLVM.WriteBitcodeToFile(mod, "sum.bc") != 0)
+            {
+                Console.WriteLine("error writing bitcode to file, skipping");
+            }
+
+            LLVM.DumpModule(mod);
+
+            LLVM.DisposeBuilder(builder);
+            LLVM.DisposeExecutionEngine(engine);
+            Console.ReadKey();
+        }
+    }
+}
