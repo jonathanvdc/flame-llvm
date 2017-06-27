2261cfc20226ccdeb8857caea1931695ebdbfff7
jonathanvdc <jonathan.vdc@outlook.com>
2017-06-27 17:54:00 +0200

    Implement some LLVMAssembly methods

--- Flame.LLVM/LLVMAssembly.cs
+++ Flame.LLVM/LLVMAssembly.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using Flame.Compiler.Build;
+using LLVMSharp;
+using static LLVMSharp.LLVM;
 
 namespace Flame.LLVM
 {
@@ -16,19 +20,33 @@ namespace Flame.LLVM
             this.Name = Name;
             this.AssemblyVersion = AssemblyVersion;
             this.Attributes = Attributes;
+            this.Module = ModuleCreateWithName(Name.ToString());
         }
 
         public Version AssemblyVersion { get; private set; }
 
         public AttributeMap Attributes { get; private set; }
 
+        /// <summary>
+        /// Gets the assembly's name.
+        /// </summary>
+        /// <returns>The assembly's name.</returns>
         public UnqualifiedName Name { get; private set; }
+
+        /// <summary>
+        /// Gets the LLVM module that is managed by this LLVM assembly.
+        /// </summary>
+        /// <returns>The LLVM module.</returns>
+        public LLVMModuleRef Module { get; private set; }
 
         public QualifiedName FullName => Name.Qualify();
 
         public IAssembly Build()
         {
-            throw new NotImplementedException();
+            IntPtr error;
+            VerifyModule(Module, LLVMVerifierFailureAction.LLVMAbortProcessAction, out error);
+            DisposeMessage(error);
+            return this;
         }
 
         public IBinder CreateBinder()
@@ -48,12 +66,19 @@ namespace Flame.LLVM
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+
         }
 
         public void Save(IOutputProvider OutputProvider)
         {
-            throw new NotImplementedException();
+            var file = OutputProvider.Create();
+            IntPtr moduleOutput = PrintModuleToString(Module);
+            var ir = Marshal.PtrToStringAnsi(moduleOutput);
+            using (var writer = new StreamWriter(file.OpenOutput()))
+            {
+                writer.Write(ir);
+            }
+            LLVMSharp.LLVM.DisposeMessage(moduleOutput);
         }
 
         public void SetEntryPoint(IMethod Method)
