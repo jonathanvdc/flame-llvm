a31b1095c7208754449fb6a6ffe1a2c572db84bb
jonathanvdc <jonathan.vdc@outlook.com>
2017-07-19 16:51:50 +0200

    Tweak the hello world test

--- tests/hello-world/hello-world.cs
+++ tests/hello-world/hello-world.cs
@@ -4,12 +4,17 @@ public static unsafe class Program
     public static extern void free(void* data);
     public static extern int puts(byte* str);
 
-    public static int Main()
+    public static void FillString(byte* str)
     {
-        byte* str = (byte*)malloc(3);
         *str = (byte)'h';
         *(str + 1) = (byte)'i';
         *(str + 2) = (byte)'\0';
+    }
+
+    public static int Main()
+    {
+        byte* str = (byte*)malloc(3);
+        FillString(str);
         puts(str);
         free((void*)str);
         return 0;
