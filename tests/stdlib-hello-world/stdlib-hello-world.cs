15b95661cbd79b77cac279bc5945d4b193d3235a
jonathanvdc <jonathan.vdc@outlook.com>
2017-07-22 21:28:37 +0200

    Add a standard library test

---
+++ tests/stdlib-hello-world/stdlib-hello-world.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.Write('h');
+        Console.Write('i');
+        Console.WriteLine();
+    }
+}
