4d39cad23705ed9e05882edb4e2b3a0944f1b000
jonathanvdc <jonathan.vdc@outlook.com>
2017-07-28 17:01:09 +0200

    Remove the GC initialization code

--- runtime/GC.cs
+++ runtime/GC.cs
@@ -7,16 +7,7 @@ namespace __compiler_rt
     /// </summary>
     public static unsafe class GC
     {
-        /// <summary>
-        /// Initializes the garbage collector.
-        /// </summary>
-        static GC()
-        {
-            GC_init();
-        }
-
         private static extern void* GC_malloc(ulong size);
-        private static extern void GC_init();
 
         /// <summary>
         /// Allocates a region of storage that is the given number of bytes in size.
