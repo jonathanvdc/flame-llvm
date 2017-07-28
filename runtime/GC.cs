daf6808bff203300fecaadaf181d33e0db83c1eb
jonathanvdc <jonathan.vdc@outlook.com>
2017-07-28 16:51:18 +0200

    Use the Boehm-Demers-Weiser GC

--- runtime/GC.cs
+++ runtime/GC.cs
@@ -5,8 +5,11 @@ namespace __compiler_rt
     /// <summary>
     /// Garbage collection functionality that can be used by the compiler.
     /// </summary>
-    public static class GC
+    public static unsafe class GC
     {
+        private static extern void* GC_malloc(ulong size);
+        private static extern void GC_init();
+
         /// <summary>
         /// Allocates a region of storage that is the given number of bytes in size.
         /// The storage is zero-initialized.
@@ -14,7 +17,15 @@ namespace __compiler_rt
         /// <param name="size">The number of bytes to allocate.</param>
         public static void* Allocate(ulong size)
         {
-            return Marshal.AllocHGlobal(size);
+            return GC_malloc(size);
+        }
+
+        /// <summary>
+        /// Initializes the garbage collector.
+        /// </summary>
+        public static void Initialize()
+        {
+            GC_init();
         }
     }
 }
