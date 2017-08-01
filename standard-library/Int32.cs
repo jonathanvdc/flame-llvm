62ff03224be369f835d5f99392bde48db5eec204
jonathanvdc <jonathan.vdc@outlook.com>
2017-08-01 13:13:07 +0200

    Fix a typo in a comment

--- standard-library/Int32.cs
+++ standard-library/Int32.cs
@@ -6,7 +6,7 @@ namespace System
     public struct Int32
     {
         // Note: integers are equivalent to instances of this data structure because
-        // flame-llvm the contents of single-field structs as a value of their single
+        // flame-llvm stores the contents of single-field structs as a value of their
         // field, rather than as a LLVM struct. So a 32-bit integer becomes an i32 and
         // so does a `System.Int32`. So don't add, remove, or edit the fields in this
         // struct.
