82f7c55b3a337de78108b2458996e19179e68b6e
jonathanvdc <jonathan.vdc@outlook.com>
2017-08-03 17:56:04 +0200

    Define a math class in the standard library

---
+++ stdlib/Math.cs
@@ -0,0 +1,10 @@
+namespace System
+{
+    /// <summary>
+    /// Defines common mathematical functions and operations.
+    /// </summary>
+    public static class Math
+    {
+        public const double PI = 3.14159265358979;
+    }
+}
