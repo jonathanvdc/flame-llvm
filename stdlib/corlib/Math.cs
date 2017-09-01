3ff8bb3e73ebf8d962faa5c7871e3b2385f4c8ce
jonathanvdc <jonathan.vdc@outlook.com>
2017-09-01 18:11:52 +0200

    Implement Math.{Min,Max} for Int32

--- stdlib/corlib/Math.cs
+++ stdlib/corlib/Math.cs
@@ -6,5 +6,27 @@ namespace System
     public static class Math
     {
         public const double PI = 3.14159265358979;
+
+        /// <summary>
+        /// Gets the largest of the given two integers.
+        /// </summary>
+        /// <param name="a">The first integer.</param>
+        /// <param name="b">The second integer.</param>
+        /// <returns><c>a</c> or <c>b</c>, whichever is larger.</returns>
+        public static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        /// <summary>
+        /// Gets the smallest of the given two integers.
+        /// </summary>
+        /// <param name="a">The first integer.</param>
+        /// <param name="b">The second integer.</param>
+        /// <returns><c>a</c> or <c>b</c>, whichever is smaller.</returns>
+        public static int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
     }
 }
