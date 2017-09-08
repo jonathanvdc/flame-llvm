35848257bfbf6f611529579513db47a6c45940af
jonathanvdc <jonathan.vdc@outlook.com>
2017-09-08 16:48:38 +0200

    Reduce code duplication in System.Convert

--- stdlib/corlib/Convert.cs
+++ stdlib/corlib/Convert.cs
@@ -57,42 +57,7 @@ namespace System
         /// <returns>A string representation for the integer.</returns>
         public static string ToString(int value)
         {
-            // Based on the code from bhuwansahni's answer to this StackOverflow question:
-            // https://stackoverflow.com/questions/9655202/how-to-convert-integer-to-string-in-c
-
-            int length = 0;
-            bool isNegative = false;
-            if (value < 0)
-            {
-                length++;
-                isNegative = true;
-                value = -value;
-            }
-
-            int shifter = value;
-            do
-            {
-                // Move to where the representation ends.
-                length++;
-                shifter /= 10;
-            } while (shifter != 0);
-
-            var array = new char[length];
-            int offset = length - 1;
-            do
-            {
-                // Move back, inserting digits as we go.
-                array[offset] = (char)(value % 10 + (int)'0');
-                offset--;
-                value /= 10;
-            } while (value != 0);
-
-            if (isNegative)
-            {
-                array[0] = '-';
-            }
-
-            return new String(array);
+            return ToString((long)value);
         }
 
         /// <summary>
