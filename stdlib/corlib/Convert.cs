00a517a05ebde7e2ecc26b1578f3f61f4c985abf
jonathanvdc <jonathan.vdc@outlook.com>
2017-09-11 20:40:30 +0200

    Define more Convert.ToString overloads

--- stdlib/corlib/Convert.cs
+++ stdlib/corlib/Convert.cs
@@ -1,3 +1,8 @@
+// This file makes use of the LeMP 'unroll' macro to avoid copy-pasting code.
+// See http://ecsharp.net/lemp/avoid-tedium-with-LeMP.html for an explanation.
+
+#importMacros(LeMP);
+
 namespace System
 {
     /// <summary>
@@ -6,7 +11,7 @@ namespace System
     public static class Convert
     {
         /// <summary>
-        /// Converts the given 64-bit signed integer to a string.
+        /// Converts a signed 64-bit signed integer to a string.
         /// </summary>
         /// <param name="value">The integer value.</param>
         /// <returns>A string representation for the integer.</returns>
@@ -51,13 +56,62 @@ namespace System
         }
 
         /// <summary>
-        /// Converts the given 32-bit signed integer to a string.
+        /// Converts an unsigned 64-bit signed integer to a string.
         /// </summary>
         /// <param name="value">The integer value.</param>
         /// <returns>A string representation for the integer.</returns>
-        public static string ToString(int value)
+        public static string ToString(ulong value)
         {
-            return ToString((long)value);
+            // Based on the code from bhuwansahni's answer to this StackOverflow question:
+            // https://stackoverflow.com/questions/9655202/how-to-convert-integer-to-string-in-c
+
+            int length = 0;
+
+            ulong shifter = value;
+            do
+            {
+                // Move to where the representation ends.
+                length++;
+                shifter /= 10;
+            } while (shifter != 0);
+
+            var array = new char[length];
+            int offset = length - 1;
+            do
+            {
+                // Move back, inserting digits as we go.
+                array[offset] = (char)(value % 10 + (int)'0');
+                offset--;
+                value /= 10;
+            } while (value != 0);
+
+            return new String(array);
+        }
+
+        unroll ((TYPE) in (sbyte, short, int))
+        {
+            /// <summary>
+            /// Converts the given signed integer value to a string.
+            /// </summary>
+            /// <param name="value">The integer value.</param>
+            /// <returns>A string representation for the integer.</returns>
+            public static string ToString(TYPE value)
+            {
+                return ToString((long)value);
+            }
+        }
+
+        unroll ((TYPE) in (byte, ushort, uint))
+        {
+            /// <summary>
+            /// Converts the given unsigned integer value to a string.
+            /// </summary>
+            /// <param name="value">The integer value.</param>
+            /// <returns>A string representation for the integer.</returns>
+            public static string ToString(TYPE value)
+            {
+                return ToString((ulong)value);
+            }
         }
 
         /// <summary>
