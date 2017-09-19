b17572f1a913d573682daf47a6388f4c4ad8f767
jonathanvdc <jonathan.vdc@outlook.com>
2017-09-19 15:56:29 +0200

    Use '{NoAlias,NoThrow}Attribute' in 'GC.cs'

--- runtime/GC.cs
+++ runtime/GC.cs
@@ -5,13 +5,18 @@ namespace __compiler_rt
     /// </summary>
     public static unsafe class GC
     {
+        [#builtin_attribute(NoAliasAttribute)]
+        [#builtin_attribute(NoThrowAttribute)]
         private static extern void* GC_malloc(ulong size);
 
         /// <summary>
-        /// Allocates a region of storage that is the given number of bytes in size.
-        /// The storage is zero-initialized.
+        /// Allocates a region of storage that is a number of bytes in size.
+        /// The storage is zero-initialized and a pointer to it is returned.
         /// </summary>
         /// <param name="size">The number of bytes to allocate.</param>
+        /// <returns>A pointer to a region of storage.</returns>
+        [#builtin_attribute(NoAliasAttribute)]
+        [#builtin_attribute(NoThrowAttribute)]
         public static void* Allocate(ulong size)
         {
             return GC_malloc(size);
