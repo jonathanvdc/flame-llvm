58cdf713b7568785aae0472052ad6ca28ec97ab5
jonathanvdc <jonathan.vdc@outlook.com>
2017-10-17 18:36:34 +0200

    Define 'System.IO.FileAccess'

---
+++ stdlib/system-io/FileAccess.cs
@@ -0,0 +1,13 @@
+namespace System.IO
+{
+    /// <summary>
+    /// Specifies how a file can be accessed. This enumeration is
+    /// a bitwise combination of flags.
+    /// </summary>
+    public enum FileAccess
+    {
+        Read = 1,
+        Write = 2,
+        ReadWrite = 3
+    }
+}
