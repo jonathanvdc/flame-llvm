436a22075a3f4ddfd6dbd5dc3ae0874077e5b008
jonathanvdc <jonathan.vdc@outlook.com>
2017-10-31 12:53:10 +0100

    Define basic threading primitives

---
+++ stdlib-primitives/Threading/ThreadingPrimitives.cs
@@ -0,0 +1,72 @@
+using pthread_t = ulong;
+using pthread_attr_t = void;
+// Actually, 'pthread_start_routine_t' has type 'void* (void*)'
+using pthread_start_routine_t = void;
+
+using ThreadStartRoutine = #builtin_delegate_type<void>;
+using pthread_start_routine_delegate = #builtin_delegate_type<ThreadStartRoutine, void*>;
+
+namespace System.Primitives.Threading
+{
+    /// <summary>
+    /// A data structure that stores a thread's unique identifier.
+    /// </summary>
+    public struct ThreadId
+    {
+        internal pthread_t id;
+    }
+
+    public static class ThreadingPrimitives
+    {
+        /// <summary>
+        /// Converts a delegate of a specified type to a function pointer that is callable from unmanaged code.
+        /// </summary>
+        /// <param name="value">The delegate to convert to a function pointer.</param>
+        /// <returns>A function pointer.</returns>
+        [#builtin_attribute(RuntimeImplementedAttribute)]
+        private static pthread_start_routine_t* LoadDelegateFunctionPointerInternal(pthread_start_routine_delegate value);
+
+        private static extern int pthread_create(
+            pthread_t* thread, pthread_attr_t* attr,
+            pthread_start_routine_t* start_routine, void* arg);
+
+        private static extern int pthread_join(
+            pthread_t thread, void* * retval);
+
+        /// <summary>
+        /// Starts a new thread with the given routine.
+        /// </summary>
+        /// <param name="startRoutine">The routine run by the thread.</param>
+        /// <param name="threadId">The thread's identifier.</param>
+        /// <returns>An error code. A nonzero exit code indicates failure.</returns>
+        public static int CreateThread(
+            ThreadStartRoutine startRoutine,
+            out ThreadId threadId)
+        {
+            threadId = default(ThreadId);
+            var startRoutinePtr = #builtin_ref_to_ptr(startRoutine);
+            return pthread_create(
+                &threadId.id,
+                (pthread_attr_t*)null,
+                LoadDelegateFunctionPointerInternal(RunStartRoutine),
+                startRoutinePtr);
+        }
+
+        /// <summary>
+        /// Waits for the thread with the given identifier to complete.
+        /// </summary>
+        /// <param name="threadId">A thread identifier.</param>
+        /// <returns>An error code. A nonzero exit code indicates failure.</returns>
+        public static int JoinThread(ThreadId threadId)
+        {
+            return pthread_join(threadId.id, (void* *)null);
+        }
+
+        private static void* RunStartRoutine(
+            ThreadStartRoutine startRoutine)
+        {
+            startRoutine();
+            return (void*)null;
+        }
+    }
+}
