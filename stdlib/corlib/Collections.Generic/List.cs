73187b33c76d2773ed3f42f806a0642b264623ba
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-01 12:50:43 +0100

    Implement 'IEnumerable<T>' in 'List<T>'

--- stdlib/corlib/Collections.Generic/List.cs
+++ stdlib/corlib/Collections.Generic/List.cs
@@ -3,7 +3,7 @@ namespace System.Collections.Generic
     /// <summary>
     /// A strongly-typed array list.
     /// </summary>
-    public class List<T> : Object
+    public class List<T> : Object, IEnumerable<T>
     {
         /// <summary>
         /// Creates an empty list.
@@ -83,6 +83,65 @@ namespace System.Collections.Generic
                 newContents[i] = contents[i];
             }
             contents = newContents;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ArrayEnumerator<T>(contents, 0, size);
+        }
+    }
+
+    internal sealed class ArrayEnumerator<T> : IEnumerator<T>
+    {
+        public ArrayEnumerator(T[] array, int startIndex, int count)
+        {
+            this.array = array;
+            this.startIndex = startIndex;
+            this.endIndex = startIndex + count;
+            this.currentIndex = startIndex;
+        }
+
+        private T[] array;
+        private int startIndex;
+        private int endIndex;
+        private int currentIndex;
+
+        private bool IsInBounds => currentIndex < endIndex;
+
+        public T Current
+        {
+            get
+            {
+                if (array == null)
+                    throw new ObjectDisposedException("this");
+                else if (IsInBounds)
+                    return array[currentIndex];
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (array == null)
+                throw new ObjectDisposedException("this");
+
+            currentIndex++;
+            return IsInBounds;
+        }
+
+        public void Dispose()
+        {
+            array = null;
+        }
+
+        public void Reset()
+        {
+            if (array == null)
+                throw new ObjectDisposedException("this");
+
+            currentIndex = startIndex;
         }
     }
 }
