3e26473ca0683e8d94457b0bbc004fe4eddc9fdc
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-01 18:24:59 +0100

    Define some array helpers

--- stdlib/corlib/Array.cs
+++ stdlib/corlib/Array.cs
@@ -1,3 +1,5 @@
+#importMacros(LeMP.CSharp6);
+
 namespace System
 {
     // TODO: tweak the compiler to make all arrays implement 'System.Array'.
@@ -5,9 +7,71 @@ namespace System
     {
         public static void Clear(byte[] array, int index, int length)
         {
+            Clear<byte>(array, index, length);
+        }
+
+        public static void Clear<T>(T[] array, int index, int length)
+        {
+            EnsureValidRange(
+                nameof(index), nameof(array),
+                index, length, array.Length);
+
             for (int i = 0; i < length; i++)
             {
-                array[index + i] = 0;
+                array[index + i] = default(T);
+            }
+        }
+
+        public static unsafe void Copy<T>(
+            T[] source, int sourceIndex,
+            T[] destination, int destinationIndex,
+            int length)
+        {
+            EnsureValidRange(
+                nameof(sourceIndex), nameof(source),
+                sourceIndex, length, source.Length);
+
+            EnsureValidRange(
+                nameof(destinationIndex), nameof(destination),
+                destinationIndex, length, destination.Length);
+
+            Buffer.MemoryCopy(
+                &source[sourceIndex],
+                &destination[destinationIndex],
+                (destination.Length - destinationIndex) * sizeof(T),
+                (length - sourceIndex) * sizeof(T));
+        }
+
+        internal static bool IsValidRange(int rangeStart, int rangeLength, int dataLength)
+        {
+            return rangeStart + rangeLength <= dataLength;
+        }
+
+        internal static void EnsureValidRange(
+            string argName, string arrayName,
+            int rangeStart, int rangeLength, int arrayLength)
+        {
+            EnsureInArray(argName, arrayName, rangeStart, arrayLength);
+
+            if (!IsValidRange(rangeStart, rangeLength, arrayLength))
+            {
+                throw new ArgumentOutOfRangeException(
+                    argName,
+                    rangeStart,
+                    arrayName + " array range [" + rangeStart + ", " + rangeStart + rangeLength + ") is out of bounds.");
+            }
+        }
+
+        internal static void EnsureInArray(
+            string argName, string arrayName,
+            int argValue, int arrayLength)
+        {
+            if (argValue >= arrayLength)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(argName),
+                    argValue,
+                    argName + " is greater than the length of the " + arrayName + " array.");
             }
         }
     }
