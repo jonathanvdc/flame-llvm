a93fb305664b16cedaca11a91a50c1dbadaa259a
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-01 20:07:39 +0100

    Test list insert, remove in stdlib-list test

--- tests/stdlib-list/stdlib-list.cs
+++ tests/stdlib-list/stdlib-list.cs
@@ -8,6 +8,8 @@ public static class Program
         var list = new List<int>();
         list.Add(10);
         list.Add(42);
+        list.Insert(0, 90);
+        list.Remove(90);
         Console.Write(list[0]);
         for (int i = 1; i < list.Count; i++)
         {
