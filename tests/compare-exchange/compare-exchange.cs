b9d21ecf6f7b0ac4f7e36fb9022dbcb34491d9ec
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-06 14:08:51 +0100

    Add a CompareExchange test

---
+++ tests/compare-exchange/compare-exchange.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+using static System.Console;
+
+public static class Program
+{
+    public static void Main()
+    {
+        int dest = 10;
+        int value = 5;
+        int comparand = 10;
+        WriteLine(Interlocked.CompareExchange(ref dest, value, comparand) + " " + dest);
+    }
+}
