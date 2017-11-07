12b4f827f71d26775dddaad0f3f74f035eba2909
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-07 13:03:41 +0100

    Implement Interlocked.Exchange

--- stdlib/corlib/Threading/Interlocked.cs
+++ stdlib/corlib/Threading/Interlocked.cs
@@ -1,3 +1,5 @@
+#importMacros(LeMP);
+
 namespace System.Threading
 {
     /// <summary>
@@ -5,101 +7,56 @@ namespace System.Threading
     /// </summary>
     public static class Interlocked
     {
-        /// <summary>
-        /// Compares two 32-bit signed integers for equality and,
-        /// if they are equal, replaces the first value.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is compared with
-        /// the comparand and possibly replaced.
-        /// </param>
-        /// <param name="value">
-        /// A value that replaces the destination value if the comparison
-        /// results in equality.
-        /// </param>
-        /// <param name="comparand">
-        /// A value that is compared to the value at the destination.
-        /// </param>
-        /// <returns>
-        /// The original value in the destination.
-        /// </returns>
-        [#builtin_attribute(RuntimeImplementedAttribute)]
-        public static int CompareExchange(
-            ref int destination,
-            int value,
-            int comparand);
+        unroll ((TYPE) in (int, long, IntPtr, object))
+        {
+            /// <summary>
+            /// Compares two values for equality and,
+            /// if they are equal, replaces the first value.
+            /// </summary>
+            /// <param name="destination">
+            /// A reference to a destination, whose value is compared with
+            /// the comparand and possibly replaced.
+            /// </param>
+            /// <param name="value">
+            /// A value that replaces the destination value if the comparison
+            /// results in equality.
+            /// </param>
+            /// <param name="comparand">
+            /// A value that is compared to the value at the destination.
+            /// </param>
+            /// <returns>
+            /// The original value in the destination.
+            /// </returns>
+            [#builtin_attribute(RuntimeImplementedAttribute)]
+            public static TYPE CompareExchange(
+                ref TYPE destination,
+                TYPE value,
+                TYPE comparand);
 
-        /// <summary>
-        /// Compares two 64-bit signed integers for equality and,
-        /// if they are equal, replaces the first value.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is compared with
-        /// the comparand and possibly replaced.
-        /// </param>
-        /// <param name="value">
-        /// A value that replaces the destination value if the comparison
-        /// results in equality.
-        /// </param>
-        /// <param name="comparand">
-        /// A value that is compared to the value at the destination.
-        /// </param>
-        /// <returns>
-        /// The original value in the destination.
-        /// </returns>
-        [#builtin_attribute(RuntimeImplementedAttribute)]
-        public static long CompareExchange(
-            ref long destination,
-            long value,
-            long comparand);
+            [#builtin_attribute(RuntimeImplementedAttribute)]
+            private static TYPE AtomicRMWXchg(
+                ref TYPE destination,
+                TYPE value);
 
-        /// <summary>
-        /// Compares two pointers for equality and,
-        /// if they are equal, replaces the first value.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is compared with
-        /// the comparand and possibly replaced.
-        /// </param>
-        /// <param name="value">
-        /// A value that replaces the destination value if the comparison
-        /// results in equality.
-        /// </param>
-        /// <param name="comparand">
-        /// A value that is compared to the value at the destination.
-        /// </param>
-        /// <returns>
-        /// The original value in the destination.
-        /// </returns>
-        [#builtin_attribute(RuntimeImplementedAttribute)]
-        public static IntPtr CompareExchange(
-            ref IntPtr destination,
-            IntPtr value,
-            IntPtr comparand);
-
-        /// <summary>
-        /// Compares two references for equality and,
-        /// if they are equal, replaces the first value.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is compared with
-        /// the comparand and possibly replaced.
-        /// </param>
-        /// <param name="value">
-        /// A value that replaces the destination value if the comparison
-        /// results in equality.
-        /// </param>
-        /// <param name="comparand">
-        /// A value that is compared to the value at the destination.
-        /// </param>
-        /// <returns>
-        /// The original value in the destination.
-        /// </returns>
-        [#builtin_attribute(RuntimeImplementedAttribute)]
-        public static object CompareExchange(
-            ref object destination,
-            object value,
-            object comparand);
+            /// <summary>
+            /// Atomically replaces the value at a particular destination.
+            /// </summary>
+            /// <param name="destination">
+            /// A reference to a destination whose value is replaced.
+            /// </param>
+            /// <param name="value">
+            /// A new value to store in the destination.
+            /// </param>
+            /// <returns>
+            /// The old value in the destination.
+            /// </returns>
+            public static TYPE Exchange(
+                ref TYPE destination,
+                TYPE value)
+            {
+                return AtomicRMWXchg(ref destination, value);
+            }
+        }
 
         /// <summary>
         /// Compares two references for equality and,
@@ -127,111 +84,87 @@ namespace System.Threading
             where T : class;
 
         [#builtin_attribute(RuntimeImplementedAttribute)]
-        private static int AtomicRMWAdd(
-            ref int destination,
-            int value);
+        private static T AtomicRMWXchg<T>(
+            ref T destination,
+            T value)
+            where T : class;
 
         /// <summary>
-        /// Adds two 32-bit signed integers and replaces the first
-        /// with the sum, in an atomic operation.
+        /// Atomically replaces the value at a particular destination.
         /// </summary>
         /// <param name="destination">
-        /// A reference to a destination, whose value is first read from
-        /// and then written to in an atomic operation.
+        /// A reference to a destination whose value is replaced.
         /// </param>
         /// <param name="value">
-        /// A value to add to the destination value.
+        /// A new value to store in the destination.
         /// </param>
         /// <returns>
-        /// The new value at the destination.
+        /// The old value in the destination.
         /// </returns>
-        public static int Add(
-            ref int destination,
-            int value)
-        {
-            return AtomicRMWAdd(ref destination, value);
-        }
-
         [#builtin_attribute(RuntimeImplementedAttribute)]
-        private static long AtomicRMWAdd(
-            ref long destination,
-            long value);
-
-        /// <summary>
-        /// Adds two 64-bit signed integers and replaces the first
-        /// with the sum, in an atomic operation.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is first read from
-        /// and then written to in an atomic operation.
-        /// </param>
-        /// <param name="value">
-        /// A value to add to the destination value.
-        /// </param>
-        /// <returns>
-        /// The new value at the destination.
-        /// </returns>
-        public static long Add(
-            ref long destination,
-            long value)
+        public static T Exchange<T>(
+            ref T destination,
+            T value)
+            where T : class
         {
-            return AtomicRMWAdd(ref destination, value);
+            return AtomicRMWXchg<T>(ref destination, value);
         }
 
-        /// <summary>
-        /// Increments a 32-bit signed integer at a particular destination,
-        /// in an atomic operation.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is first read and then written to
-        /// in an atomic operation.
-        /// </param>
-        /// <returns>The new value at the destination.</returns>
-        public static int Increment(ref int destination)
+        unroll ((TYPE) in (int, long))
         {
-            return Add(ref destination, 1);
-        }
+            [#builtin_attribute(RuntimeImplementedAttribute)]
+            private static int AtomicRMWAdd(
+                ref TYPE destination,
+                TYPE value);
 
-        /// <summary>
-        /// Increments a 64-bit signed integer at a particular destination,
-        /// in an atomic operation.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is first read and then written to
-        /// in an atomic operation.
-        /// </param>
-        /// <returns>The new value at the destination.</returns>
-        public static long Increment(ref long destination)
-        {
-            return Add(ref destination, 1);
-        }
+            /// <summary>
+            /// Adds two values and replaces the first
+            /// with the sum, in an atomic operation.
+            /// </summary>
+            /// <param name="destination">
+            /// A reference to a destination, whose value is first read from
+            /// and then written to in an atomic operation.
+            /// </param>
+            /// <param name="value">
+            /// A value to add to the destination value.
+            /// </param>
+            /// <returns>
+            /// The new value at the destination.
+            /// </returns>
+            public static TYPE Add(
+                ref TYPE destination,
+                TYPE value)
+            {
+                return AtomicRMWAdd(ref destination, value);
+            }
 
-        /// <summary>
-        /// Decrements a 32-bit signed integer at a particular destination,
-        /// in an atomic operation.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is first read and then written to
-        /// in an atomic operation.
-        /// </param>
-        /// <returns>The new value at the destination.</returns>
-        public static int Decrement(ref int destination)
-        {
-            return Add(ref destination, -1);
-        }
+            /// <summary>
+            /// Increments a value at a particular destination,
+            /// in an atomic operation.
+            /// </summary>
+            /// <param name="destination">
+            /// A reference to a destination, whose value is first
+            /// read and then written to in an atomic operation.
+            /// </param>
+            /// <returns>The new value at the destination.</returns>
+            public static TYPE Increment(ref TYPE destination)
+            {
+                return Add(ref destination, 1);
+            }
 
-        /// <summary>
-        /// Decrements a 64-bit signed integer at a particular destination,
-        /// in an atomic operation.
-        /// </summary>
-        /// <param name="destination">
-        /// A reference to a destination, whose value is first read and then written to
-        /// in an atomic operation.
-        /// </param>
-        /// <returns>The new value at the destination.</returns>
-        public static long Decrement(ref long destination)
-        {
-            return Add(ref destination, -1);
+            /// <summary>
+            /// Decrements a value at a particular destination,
+            /// in an atomic operation.
+            /// </summary>
+            /// <param name="destination">
+            /// A reference to a destination, whose value is first
+            /// read and then written to in an atomic operation.
+            /// </param>
+            /// <returns>The new value at the destination.</returns>
+            public static TYPE Decrement(ref TYPE destination)
+            {
+                return Add(ref destination, -1);
+            }
         }
     }
 }
